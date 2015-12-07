be61d2cbd8068b354332ec480cdce20669bb6133
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-07 05:07:36 +0100

    refactor Day6 to do part1 and part2 at once

--- src/Day6.scala
+++ src/Day6.scala
@@ -2,42 +2,27 @@
 object Day6 extends App {
   val ActionRE = "([a-z\\s]+) (\\d+),(\\d+) through (\\d+),(\\d+)".r
 
-  println(s"PART 1: total ${run(part1)} lights are lit")
-  println(s"PART 2: total brightness: ${run(part2)}")
+  val matrix = Array.fill(1000, 1000)((false, 0))
 
-  private def run(callback: (String, Int) => Int): Int = {
-    val matrix = Array.fill(1000, 1000)(0)
-
-    Input(6).lines.foreach {
-      case ActionRE(action, startX, startY, endX, endY) =>
-        (startX.toInt to endX.toInt).foreach { x =>
-          (startY.toInt to endY.toInt).foreach { y =>
-            matrix(x)(y) = callback(action, matrix(x)(y))
+  Input(6).lines.foreach {
+    case ActionRE(action, startX, startY, endX, endY) =>
+      (startX.toInt to endX.toInt).foreach { x =>
+        (startY.toInt to endY.toInt).foreach { y =>
+          matrix(x)(y) = {
+            val value = matrix(x)(y)
+            action match {
+              case "turn on" => (true, value._2 + 1)
+              case "turn off" => (false, value._2 match {
+                case 0 => 0
+                case v => v - 1
+              })
+              case "toggle" => (!value._1, value._2 + 2)
+            }
           }
         }
-    }
-
-    matrix.map(_.sum).sum
+      }
   }
 
-  private def part1(action: String, value: Int): Int =
-    action match {
-      case "turn on" => 1
-      case "turn off" => 0
-      case "toggle" => value match {
-        case 1 => 0
-        case 0 => 1
-        }
-    }
-
-
-  private def part2(action: String, value: Int): Int =
-    action match {
-      case "turn on" => value + 1
-      case "turn off" => value match {
-        case 0 => 0
-        case v => v - 1
-      }
-      case "toggle" => value + 2
-    }
+  println(s"PART 1: total ${matrix.map(_.count(_._1)).sum} lights are lit")
+  println(s"PART 2: total brightness: ${matrix.map(_.map(_._2).sum).sum}")
 }
