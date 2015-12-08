50c2f0ce70bd065b87c34a5490fa45f8f5b57d91
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-08 02:33:44 +0100

    day 7 first working version ...

---
+++ src/Day7.scala
@@ -0,0 +1,45 @@
+object Day7 extends App {
+  val InstructionRE = "([\\w ]+) -> (\\w+)".r
+  val SignalRE = "(\\d+)".r
+  val WireRE = "(\\w+)".r
+  val AndRE = "(\\w+) AND (\\w+)".r
+  val OrRE = "(\\w+) OR (\\w+)".r
+  val LShiftRE = "(\\w+) LSHIFT (\\d+)".r
+  val RShiftRE = "(\\w+) RSHIFT (\\d+)".r
+  val NotRE = "NOT (\\w+)".r
+
+  var wires = Input(7).lines.map {
+    case InstructionRE(inputs, output) => output -> inputs
+  }.toMap
+
+  var signals = Map.empty[String, Int]
+
+  def input(wire: String): Int =
+    signals.getOrElse(wire, {
+      implicit def stringToInt(x: String): Int = augmentString(x).toInt
+
+      wire match {
+        case SignalRE(in) => in
+        case wire: String => {
+          val signal: Int = wires(wire) match {
+            case WireRE(in) => input(in)
+            case AndRE(in1, in2) => input(in1) & input(in2)
+            case OrRE(in1, in2) => input(in1) | input(in2)
+            case LShiftRE(in, positions) => input(in) << positions
+            case RShiftRE(in, positions) => input(in) >> positions
+            case NotRE(in) => ~input(in)
+          }
+          signals += wire -> signal
+          signal
+        }
+      }
+    })
+
+  val part1 = input("a")
+
+  wires += ("b" -> part1.toString)
+  signals = Map.empty[String, Int]
+  val part2 = input("a")
+
+  println(s"PART 1: $part1 | PART 2: $part2")
+}
