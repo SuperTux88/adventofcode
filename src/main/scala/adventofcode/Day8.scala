cf3a155c1c7c5d8074ef27e9beb3a48e788c1853
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-09 01:52:57 +0100

    day 8

---
+++ src/main/scala/adventofcode/Day8.scala
@@ -0,0 +1,34 @@
+package adventofcode
+
+object Day8 extends App {
+  val AsciiRE = "(\\\\x..|\\\\\"|\\\\\\\\)".r
+  val charsToEscape = Seq('\\', '"')
+
+  val total1 = Input(8).lines.map { line =>
+    line.length - AsciiRE.replaceAllIn(line, " ").length + 2
+  }.sum
+
+  val total1x = Input(8).lines.map(_.toCharArray).map { line =>
+    var count = 0
+    val it = line.iterator
+    while(it.hasNext) {
+      it.next() match {
+        case '\\' => count += (it.next() match {
+            case 'x' => it.next(); it.next(); 3
+            case _ => 1
+          })
+        case '"' => count += 1
+        case _ =>
+      }
+    }
+    count
+  }.sum
+
+  val total2 = Input(8).lines.map { line =>
+    line.count(charsToEscape.contains(_)) + 2
+  }.sum
+
+  println(s"PART 1: $total1")
+  println(s"PART 1: $total1x")
+  println(s"PART 2: $total2")
+}
