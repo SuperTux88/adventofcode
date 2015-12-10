c45e883ab119caccceae75483f7418c5ae2a0959
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-10 04:00:18 +0100

    write wrapper app for single execution and benchmarks

--- src/main/scala/adventofcode/Main.scala
+++ src/main/scala/adventofcode/Main.scala
@@ -1,26 +1,94 @@
 package adventofcode
 
+import scala.io.StdIn
+
 object Main extends App {
-  val days = List(Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8)
+  val allDays = List(Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9)
 
   Logging.debug = false
 
-  // single run
-  //days.foreach { day =>
-  //  day.main(args)
-  //}
+  print(
+    """Advent of Code 2015
+      |===================
+      |
+      |  1) run and print result (default)
+      |  2) run benchmarks
+      |
+      |Select: """.stripMargin)
 
-  // benchmark
-  Logging.results = false
-  days.foreach { day =>
-    println(day.getClass.getCanonicalName.dropRight(1))
-    val times = (1 to 100).map { pass =>
-      val start = System.nanoTime
-      day.main(args)
-      (System.nanoTime - start).toFloat / 1000 / 1000
+  readInput {
+    case "1"|"" => Some(ResultMode)
+    case "2" => Some(BenchmarkMode)
+    case _ => None
+  }.run()
+
+  private def readInput[T](matchInput: String => Option[T]) = {
+    var result: Option[T] = None
+    while (result.isEmpty) {
+      result = matchInput(StdIn.readLine())
+      if (result.isEmpty) print("Invalid Input, please try again: ")
     }
-    println(f"min: ${times.min}%.3fms | avg: ${times.sum / times.size}%.3fms | max: ${times.max}%.3fms | total: ${times.sum}%.3fms")
-    //println(times.map(time => f"$time%.3f").mkString(", "))
+    println()
+    result.get
   }
 
+  private trait DaySelectorRunnable extends Runnable {
+    def run() {
+      print("Select day number or \"all\" (default: all): ")
+
+      val daysToRun = readInput {
+        case Int(dayNumber) if dayNumber > 0 && dayNumber <= allDays.size => Some(List(allDays(dayNumber -1)))
+        case "all"|"" => Some(allDays)
+        case _ => None
+      }
+
+      runDays(daysToRun)
+    }
+
+    def runDays(days: List[DayApp])
+  }
+
+  private object ResultMode extends DaySelectorRunnable {
+    def runDays(days: List[DayApp]) {
+      days.foreach { day =>
+        day.main(args)
+      }
+    }
+  }
+
+  private object BenchmarkMode extends DaySelectorRunnable {
+    def runDays(days: List[DayApp]) {
+      Logging.results = false
+
+      print("Number of runs (default: 100): ")
+
+      val runs = readInput {
+        case Int(number) if number > 0 => Some(number)
+        case "" => Some(100)
+        case _ => None
+      }
+
+      days.foreach { day =>
+        print(s"${day.getClass.getSimpleName.dropRight(1)}: ")
+
+        val times = (1 to runs).map { pass =>
+          val start = System.nanoTime
+          day.main(args)
+          (System.nanoTime - start).toFloat / 1000 / 1000
+        }
+
+        println(f"min: ${times.min}%.3fms | avg: ${times.sum / times.size}%.3fms | max: ${times.max}%.3fms | total: ${times.sum}%.3fms")
+        if (util.Properties.propIsSet("benchmark.times"))
+          println(times.map(time => f"$time%.3f").mkString(", "))
+      }
+    }
+  }
+
+  private object Int {
+    def unapply(s: String): Option[Int] = try {
+      Some(s.toInt)
+    } catch {
+      case _: java.lang.NumberFormatException => None
+    }
+  }
 }
