919f9c285a5dc059fa6bf1bc05dae0c0c2a9d7d9
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-12 04:10:31 +0100

    day 10

---
+++ src/main/scala/adventofcode/Day10.scala
@@ -0,0 +1,42 @@
+package adventofcode
+
+object Day10 extends DayApp {
+  override val day: Int = 10
+
+  val sameDigitsRE = """(\w)\1*""".r
+
+  val part1 = transform(input.mkString, 40)
+  printDayPart(1, part1.length)
+  printDayPart(2, transform(part1, 10).length)
+
+  private def transform(inputStr: String, iterations: Int) = {
+    var string = inputStr
+    (1 to iterations).foreach { i =>
+      string = transformOnce(string)
+    }
+    string
+  }
+
+  // slow
+  private def transformOnceRegex(string: String) =
+    sameDigitsRE.replaceAllIn(string, m => {
+      m.matched.length.toString + m.matched.charAt(0)
+    })
+
+  private def transformOnce(str: String) = {
+    val ret = new StringBuilder
+    var currentDigit = str.charAt(0)
+    var counter = 0
+    str.foreach { d =>
+      if(currentDigit == d) {
+        counter += 1
+      } else {
+        ret ++= counter.toString + currentDigit
+        currentDigit = d
+        counter = 1
+      }
+    }
+    ret ++= counter.toString + currentDigit
+    ret.toString()
+  }
+}
