b44a6bbf47b738a0c459ce247970d514727d10eb
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-13 19:22:40 +0100

    day 11

---
+++ src/main/scala/adventofcode/Day11.scala
@@ -0,0 +1,39 @@
+package adventofcode
+
+object Day11 extends DayApp {
+  override val day: Int = 11
+
+  val part1 = nextValidPassword(input.mkString)
+  printDayPart(1, s"next valid password: $part1")
+  printDayPart(2, s"next valid password: ${nextValidPassword(part1)}")
+
+  private def nextValidPassword(oldPassword: String) = {
+    var reversePassword = oldPassword.toList.reverse
+
+    do {
+      reversePassword = nextPassword(reversePassword)
+    } while (!containsIncreasingStraightOfThreeChars(reversePassword) || !containsTwoPairs(reversePassword))
+
+    reversePassword.reverse.mkString
+  }
+
+  private def nextPassword(reversePassword: List[Char]): List[Char] = reversePassword match {
+    case 'z' :: rest => 'a' :: nextPassword(rest)
+    case c :: rest if c == 'h' || c == 'k' || c == 'n' => (c + 2).toChar :: rest
+    case c :: rest => (c + 1).toChar :: rest
+    case Nil => List('a')
+  }
+
+  private def containsIncreasingStraightOfThreeChars(reversePassword: List[Char]) = {
+    reversePassword.sliding(3).exists { case Seq(a, b, c) =>
+      a == b + 1 && a == c + 2
+    }
+  }
+
+  private def containsTwoPairs(reversePassword: List[Char]) = {
+    val pairIndexes = reversePassword.sliding(2).zipWithIndex.collect {
+      case (pair, index) if pair.head == pair.last  => index
+    }.toSeq
+    pairIndexes.length >= 2 && pairIndexes.head < pairIndexes.last -1
+  }
+}
