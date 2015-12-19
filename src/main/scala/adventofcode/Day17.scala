723e4e5b28d34ff1c1e770b1169f3ca7bfad705e
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-20 00:29:05 +0100

    day 17

---
+++ src/main/scala/adventofcode/Day17.scala
@@ -0,0 +1,13 @@
+package adventofcode
+
+object Day17 extends DayApp {
+  override val day: Int = 17
+
+  val containers = input.getLines().map(_.toInt).toList
+
+  val allCombinations = (1 to containers.size).flatMap(containers.zipWithIndex.combinations)
+  val fullCombinations = allCombinations.filter(_.map(_._1).sum == 150)
+
+  printDayPart(1, fullCombinations.size)
+  printDayPart(2, fullCombinations.count(_.size == fullCombinations.head.size))
+}
