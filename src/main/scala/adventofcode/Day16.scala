9563053ea2c5a890475318354f91e0f815e889ec
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-19 22:03:39 +0100

    day 16

---
+++ src/main/scala/adventofcode/Day16.scala
@@ -0,0 +1,43 @@
+package adventofcode
+
+object Day16 extends DayApp {
+  override val day: Int = 16
+
+  val SueRE = """Sue (\d+):(.*)""".r
+  val CompoundRE = """ (\w+): (\d+)""".r
+
+  val searchedSueCompounds = Map(
+    "children" -> 3,
+    "cats" -> 7,
+    "samoyeds" -> 2,
+    "pomeranians" -> 3,
+    "akitas" -> 0,
+    "vizslas" -> 0,
+    "goldfish" -> 5,
+    "trees" -> 3,
+    "cars" -> 2,
+    "perfumes" -> 1
+  )
+
+  val sues = input.getLines().map {
+    case SueRE(number, compounds) =>
+      Sue(number.toInt, compounds.split(',').map {
+        case CompoundRE(thing, value) => thing -> value.toInt
+      }.toMap)
+  }.toList
+
+  printDayPart(1, sues.find(_.matchesCompounds(searchedSueCompounds)).get.number)
+  printDayPart(2, sues.find(_.isRealSue(searchedSueCompounds)).get.number)
+
+  case class Sue(number: Int, compounds: Map[String, Int]) {
+    def matchesCompounds(searchedCompounds: Map[String, Int]) =
+      !compounds.exists(c => searchedCompounds(c._1) != c._2)
+
+    def isRealSue(searchedCompounds: Map[String, Int]) =
+      !compounds.exists(c => c._1 match {
+        case "cats" | "trees" => searchedCompounds(c._1) >= c._2
+        case "pomeranians" | "goldfish" => searchedCompounds(c._1) <= c._2
+        case _ => searchedCompounds(c._1) != c._2
+      })
+  }
+}
--- src/main/scala/adventofcode/Main.scala
+++ src/main/scala/adventofcode/Main.scala
@@ -3,7 +3,7 @@ package adventofcode
 import scala.io.StdIn
 
 object Main extends App {
-  val allDays = List(Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10, Day11, Day12, Day13, Day14, Day15)
+  val allDays = List(Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10, Day11, Day12, Day13, Day14, Day15, Day16)
 
   Logging.debug = false
 
