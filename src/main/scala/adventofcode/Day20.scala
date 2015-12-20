a07ad9a6c6878b53db6c7afb31dad0ab028dd8d4
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-20 17:22:03 +0100

    day 20

---
+++ src/main/scala/adventofcode/Day20.scala
@@ -0,0 +1,24 @@
+package adventofcode
+
+object Day20 extends DayApp {
+  override val day: Int = 20
+
+  val targetNumberOfPresents = input.mkString.toInt
+  val targetPart1 = targetNumberOfPresents / 10
+  val targetPart2 = targetNumberOfPresents / 11
+  val max = targetNumberOfPresents / 40 // maybe bigger max value?
+
+  var houses = Map.empty[Int, Int].withDefaultValue(0)
+  for (elf <- 1 to max;
+       house <- elf to max by elf) {
+    houses += house -> (houses(house) + elf)
+  }
+  printDayPart(1, (1 to max).find(houses(_) > targetPart1).get)
+
+  houses = Map.empty[Int, Int].withDefaultValue(0)
+  for (elf <- 1 to max;
+       house <- (elf to max by elf).take(50)) {
+    houses += house -> (houses(house) + elf)
+  }
+  printDayPart(2, (1 to max).find(houses(_) > targetPart2).get)
+}
