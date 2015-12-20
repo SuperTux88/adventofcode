ad5bdbe0a85c217684d417647a0abbf5c7d79646
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-20 02:37:53 +0100

    day 18

---
+++ src/main/scala/adventofcode/Day18.scala
@@ -0,0 +1,44 @@
+package adventofcode
+
+object Day18 extends DayApp {
+  override val day: Int = 18
+
+  val initialGrid = input.getLines().zipWithIndex.flatMap {
+    case (line, x) => line.zipWithIndex.map {
+      case ('#', y) => (x, y) -> true
+      case ('.', y) => (x, y) -> false
+    }
+  }.toMap.withDefaultValue(false)
+
+  val gridAfter100Steps = (1 to 100).foldLeft(initialGrid) {
+    (currentGrid, step) => nextGrid(currentGrid)
+  }
+  printDayPart(1, gridAfter100Steps.values.count(identity))
+
+  val gridWithStuckLightsAfter100Steps = (1 to 100).foldLeft(withStuckLights(initialGrid)) {
+    (currentGrid, step) => withStuckLights(nextGrid(currentGrid))
+  }
+  printDayPart(2, gridWithStuckLightsAfter100Steps.values.count(identity))
+
+  def neighbors(x: Int, y: Int) =
+    for {
+      checkX <- (-1 to 1).map(_ + x)
+      checkY <- (-1 to 1).map(_ + y)
+      if checkX != x || checkY != y
+    } yield (checkX, checkY)
+
+  def nextState(on: Boolean, neighbors: Int) = (on, neighbors) match {
+    case (true, 2) | (true, 3) | (false, 3) => true
+    case _ => false
+  }
+
+  def nextGrid(currentGrid: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = {
+    for {
+      x <- 0 to 99
+      y <- 0 to 99
+    } yield (x, y) -> nextState(currentGrid(x, y), neighbors(x, y).count(currentGrid))
+  }.toMap.withDefaultValue(false)
+
+  def withStuckLights(grid: Map[(Int, Int), Boolean]) =
+    grid + ((0, 0) -> true) + ((0, 99) -> true) + ((99, 99) -> true) + ((99, 0) -> true)
+}
