4aef9440794a43183fb017c09915cb51daceb26a
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-25 04:26:59 +0100

    day 23

---
+++ src/main/scala/adventofcode/Day23.scala
@@ -0,0 +1,44 @@
+package adventofcode
+
+object Day23 extends DayApp {
+  override val day: Int = 23
+
+  val CommandRE = """(\w+) ([ab])""".r
+  val JumpCommandRE = """jmp ([+-]\d+)""".r
+  val JumpIfCommandRE = """(ji[oe]) ([ab]), ([+-]\d+)""".r
+
+  val commands = input.getLines().map {
+    case CommandRE(command, register) => Command(command, Some(register(0)), None)
+    case JumpCommandRE(offset) => Command("jmp", None, Some(offset.toInt))
+    case JumpIfCommandRE(command, register, offset) => Command(command, Some(register(0)), Some(offset.toInt))
+  }.toList
+
+  printDayPart(1, runCommands(Map())('b').toInt)
+  printDayPart(2, runCommands(Map('a' -> 1l))('b').toInt)
+
+  def runCommands(initialRegisters: Map[Char, Long]) = {
+    var registers = initialRegisters.withDefaultValue(0l)
+    var currentCommand = 0
+
+    while(currentCommand >= 0 && currentCommand < commands.size) {
+      commands(currentCommand) match {
+        case Command("hlf", Some(reg), _) =>
+          registers += reg -> registers(reg) / 2
+          currentCommand += 1
+        case Command("tpl", Some(reg), _) =>
+          registers += reg -> registers(reg) * 3
+          currentCommand += 1
+        case Command("inc", Some(reg), _) =>
+          registers += reg -> (registers(reg) + 1)
+          currentCommand += 1
+        case Command("jmp", _, Some(offset)) => currentCommand += offset
+        case Command("jie", Some(reg), Some(offset)) => currentCommand += (if(registers(reg) % 2 == 0) offset else 1)
+        case Command("jio", Some(reg), Some(offset)) => currentCommand += (if(registers(reg) == 1) offset else 1)
+      }
+    }
+
+    registers
+  }
+
+  case class Command(command: String, register: Option[Char], offset: Option[Int])
+}
