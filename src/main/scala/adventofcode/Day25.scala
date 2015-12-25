344b7e89425801e296e9133d501850aa57a96b77
Benjamin Neff <benjamin@coding4coffee.ch>
2015-12-25 20:17:15 +0100

    faster day 25 :)

--- src/main/scala/adventofcode/Day25.scala
+++ src/main/scala/adventofcode/Day25.scala
@@ -9,13 +9,13 @@ object Day25 extends DayApp {
     case InputRE(rowStr, colStr) => (rowStr.toInt, colStr.toInt)
   }
 
-  val startCode = (2 to indexOf(row, col)).foldLeft(BigInt(20151125))((code, index) => code * 252533 % 33554393)
+  val startCode = 20151125 * BigInt(252533).modPow(indexOf(row, col), 33554393) % 33554393
 
   printDayPart(1, startCode.toInt, "the start code is: %s")
 
   def indexOf(row: Int, col: Int) = {
     val maxRow = row + col - 1
     val topRight = maxRow * (maxRow+1) / 2
-    topRight - row + 1
+    topRight - row
   }
 }
