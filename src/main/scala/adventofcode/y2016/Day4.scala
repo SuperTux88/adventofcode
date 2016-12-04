857a9eebb391d0ad9f933024c1a754fea9fcc80e
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-04 06:42:23 +0100

    day 4 2016

---
+++ src/main/scala/adventofcode/y2016/Day4.scala
@@ -0,0 +1,31 @@
+package adventofcode.y2016
+
+object Day4 extends Year2016 {
+  override val day: Int = 4
+
+  val RoomRE = """([\w-]+)-(\d+)\[(\w{5})\]""".r
+
+  private val realRooms = input.getLines.map {
+    case RoomRE(name, sectorId, checksum) => Room(name, sectorId.toInt, checksum)
+  }.filter(_.isReal).toList
+
+  printDayPart(1, realRooms.map(_.sectorId).sum)
+  printDayPart(2, realRooms.find(_.decrypt == "northpole object storage").get.sectorId)
+
+  private case class Room(name: String, sectorId: Int, checksum: String) {
+    def isReal: Boolean = {
+      val calculatedChecksum = name.filter(_ != '-').groupBy(identity).mapValues(_.length).toList
+        .sortBy(_._1).sortBy(-_._2).take(5).map(_._1).mkString
+      calculatedChecksum == checksum
+    }
+
+    def decrypt: String = {
+      val key = sectorId % 26
+      name.map {
+        case '-' => ' '
+        case c if c + key <= 'z' => (c + key).toChar
+        case c if c + key  > 'z' => (c + key - 26).toChar
+      }.mkString
+    }
+  }
+}
