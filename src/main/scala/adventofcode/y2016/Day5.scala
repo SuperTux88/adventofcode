7ac27a6c40bbe409fb61d7d802655319c0825c76
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-05 22:24:30 +0100

    MOAR cinematic "decrypting" animation!!!111

--- src/main/scala/adventofcode/y2016/Day5.scala
+++ src/main/scala/adventofcode/y2016/Day5.scala
@@ -5,37 +5,51 @@ import java.security.MessageDigest
 import adventofcode.Logging
 
 import scala.annotation.tailrec
+import scala.util.Random
 
 object Day5 extends Year2016 {
   override val day: Int = 5
 
   val doorId = input.mkString
 
-  val first8Chars = (1 to 8).scanLeft((0, 0, 0))((state, _) => nextChar(doorId, state._3)).tail
+  val passwordArray = Array.fill[Option[Char]](8)(None)
+  val passwordArray2 = Array.fill[Option[Char]](8)(None)
 
+  val first8Chars = (0 to 7).scanLeft((0, 0, 0)) { (state, position) =>
+    val char = nextChar(doorId, state._3, passwordArray)
+    addCharToPassword(passwordArray, position, char._1)
+    char
+  }.tail
+
+  print("\r")
   printDayPart(1, first8Chars.map(_._1.toHexString).mkString, "password: %s")
 
-  val passwordArray = Array.fill[Option[Char]](8)(None)
   first8Chars.foreach { char =>
-    addCharToPassword(passwordArray, char._1, char._2)
-    if (Logging.debug) Thread.sleep(500)
+    addCharToPassword(passwordArray2, char._1, char._2)
+    if (Logging.results) (1 to 15).foreach { _ =>
+      printPassword(passwordArray2)
+      Thread.sleep(40)
+    }
   }
 
-  val password = findSecondPassword(passwordArray, doorId, first8Chars.last._3)
+  val password2 = findSecondPassword(passwordArray2, doorId, first8Chars.last._3)
   print("\r")
-  printDayPart(2, password, "password for second door: %s")
+  printDayPart(2, password2, "password for second door: %s")
 
   @tailrec
-  private def nextChar(doorId: String, counter: Int): (Int, Int, Int) = {
+  private def nextChar(doorId: String, counter: Int, password: Array[Option[Char]]): (Int, Int, Int) = {
     val md5 = MessageDigest.getInstance("MD5").digest((doorId + counter).getBytes)
+
+    if (Logging.results && counter % 50000 == 0) printPassword(password)
+
     if (md5(0) == 0 && md5(1) == 0 && (md5(2) & 0xf0) == 0) {
       (md5(2).toInt, md5(3) >> 4, counter + 1)
-    } else nextChar(doorId, counter + 1)
+    } else nextChar(doorId, counter + 1, password)
   }
 
   @tailrec
   private def findSecondPassword(password: Array[Option[Char]], doorId: String, counter: Int): String = {
-    val (position, char, newCounter) = nextChar(doorId, counter)
+    val (position, char, newCounter) = nextChar(doorId, counter, password)
     addCharToPassword(password, position, char)
 
     if (password.exists(_.isEmpty)) findSecondPassword(password, doorId, newCounter)
@@ -50,9 +64,9 @@ object Day5 extends Year2016 {
   }
 
   private def printPassword(password: Array[Option[Char]]) {
-    if (Logging.debug) {
+    if (Logging.results) {
       val string = password.map {
-        case None => "_"
+        case None => Random.nextInt(15).toHexString
         case Some(char) => char
       }.mkString
       print(s"\r$string")
