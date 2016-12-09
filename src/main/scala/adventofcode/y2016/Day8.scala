023749dbbb3b22a400aab06c9b08d87f36dbe28d
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-09 02:20:37 +0100

    day 8 2016 add hardcoded OCR

--- src/main/scala/adventofcode/y2016/Day8.scala
+++ src/main/scala/adventofcode/y2016/Day8.scala
@@ -24,8 +24,32 @@ object Day8 extends Year2016 {
 
   printDayPart(1, screen.map(_.count(identity)).sum, "total %s pixels are lit")
 
-  printDayPart(2, "ZJHRKCPLYJ", "the code is:")
-  if (Logging.results) screen.foreach(line => println(line.map(if (_) "█" else " ").mkString))
+  val chars = (0 to 9).map(pos => getCharAt(screen, pos))
+
+  printDayPart(2, chars.map(OCR.readChar).mkString, "the code is: %s")
+  if (Logging.debug) screen.foreach(line => println(line.map(if (_) "█" else " ").mkString))
 
   private def rotate(row: Array[Boolean], by: Int) = row.takeRight(by) ++ row.dropRight(by)
+
+  private def getCharAt(screen: Array[Array[Boolean]], position: Int) = {
+    val start = position * 5
+    screen.map(line => line.slice(start, start + 5).toList).toList
+  }
+
+  private object OCR {
+    def readChar(char: List[List[Boolean]]): Char = chars.getOrElse(char, '_')
+
+    // hardcoded chars for my solution
+    private val chars = Map(
+      'C' -> List(List(false, true, true, false, false), List(true, false, false, true, false), List(true, false, false, false, false), List(true, false, false, false, false), List(true, false, false, true, false), List(false, true, true, false, false)),
+      'H' -> List(List(true, false, false, true, false), List(true, false, false, true, false), List(true, true, true, true, false), List(true, false, false, true, false), List(true, false, false, true, false), List(true, false, false, true, false)),
+      'J' -> List(List(false, false, true, true, false), List(false, false, false, true, false), List(false, false, false, true, false), List(false, false, false, true, false), List(true, false, false, true, false), List(false, true, true, false, false)),
+      'K' -> List(List(true, false, false, true, false), List(true, false, true, false, false), List(true, true, false, false, false), List(true, false, true, false, false), List(true, false, true, false, false), List(true, false, false, true, false)),
+      'L' -> List(List(true, false, false, false, false), List(true, false, false, false, false), List(true, false, false, false, false), List(true, false, false, false, false), List(true, false, false, false, false), List(true, true, true, true, false)),
+      'P' -> List(List(true, true, true, false, false), List(true, false, false, true, false), List(true, false, false, true, false), List(true, true, true, false, false), List(true, false, false, false, false), List(true, false, false, false, false)),
+      'R' -> List(List(true, true, true, false, false), List(true, false, false, true, false), List(true, false, false, true, false), List(true, true, true, false, false), List(true, false, true, false, false), List(true, false, false, true, false)),
+      'Y' -> List(List(true, false, false, false, true), List(true, false, false, false, true), List(false, true, false, true, false), List(false, false, true, false, false), List(false, false, true, false, false), List(false, false, true, false, false)),
+      'Z' -> List(List(true, true, true, true, false), List(false, false, false, true, false), List(false, false, true, false, false), List(false, true, false, false, false), List(true, false, false, false, false), List(true, true, true, true, false))
+    ).map(_.swap)
+  }
 }
