960a7f5e2b3a5617e76c60d5cc78d73f55b41c68
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-16 05:55:59 +0100

    day 15 2016 with chinese remainder theorem

--- src/main/scala/adventofcode/y2016/Day15.scala
+++ src/main/scala/adventofcode/y2016/Day15.scala
@@ -6,26 +6,39 @@ object Day15 extends Year2016 {
   val DiscRE = """Disc #(\d) has (\d+) positions; at time=0, it is at position (\d+).""".r
 
   private val discs = input.getLines.map {
-    case DiscRE(discNo, positions, currentPos) =>
-      (1 to discNo.toInt).foldLeft(Disc(positions.toInt, currentPos.toInt))((disc, _) => disc.rotate)
+    case DiscRE(discNo, positions, currentPos) => Disc(discNo.toInt, positions.toInt, currentPos.toInt)
   }.toList
 
-  printDayPart(1, solve(discs))
-  printDayPart(2, solve(Disc(11, discs.length + 1) :: discs))
+  printDayPart(1, chineseRemainderTheorem(discs))
+  printDayPart(2, chineseRemainderTheorem(Disc(discs.length + 1, 11, 0) :: discs))
 
-  private def solve(discs: List[Disc]) = {
-    var startTime = 0
-    var currentDiscs = discs
+  private def chineseRemainderTheorem(discs: List[Disc]) = {
+    val max = discs.map(_.positions).product
+    val sum = discs.map { disc =>
+      disc.remainder * max * multiplicativeInverse(max/disc.positions, disc.positions) / disc.positions
+    }.sum
 
-    while(currentDiscs.exists(_.currentPos != 0)) {
-      currentDiscs = currentDiscs.map(_.rotate)
-      startTime += 1
-    }
-
-    startTime
+    sum % max
   }
 
-  private case class Disc(positions: Int, currentPos: Int) {
-    def rotate: Disc = copy(currentPos = (currentPos + 1) % positions)
+  private def multiplicativeInverse(r: Int, m: Int) = {
+    var x = (0, 1)
+    var mod = m
+    var remainder = r
+
+    while (remainder != 0) {
+      val quotient = mod / remainder
+      x = (x._2, x._1 - quotient * x._2)
+
+      val tmp = mod
+      mod = remainder
+      remainder = tmp % remainder
+    }
+
+    if (x._1 < 0) x._1 + m else x._1
+  }
+
+  private case class Disc(discNo: Int, positions: Int, startPos: Int) {
+    def remainder: Int = (positions - startPos - discNo) % positions
   }
 }
