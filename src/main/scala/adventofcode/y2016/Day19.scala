470c877452e67a7253c442877658eb2de8265131
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-19 08:03:28 +0100

    day 19 2016

---
+++ src/main/scala/adventofcode/y2016/Day19.scala
@@ -0,0 +1,20 @@
+package adventofcode.y2016
+
+object Day19 extends Year2016 {
+  override val day = 19
+
+  val elves = input.mkString.toInt
+
+  printDayPart(1, josephus(elves))
+  printDayPart(2, part2(elves))
+
+  private def josephus(elves: Int) =
+    Integer.parseInt(elves.toBinaryString.substring(1) + 1, 2)
+
+  private def part2(elves: Int) = {
+    var counter = 1
+    while (counter * 3 < elves) counter *= 3
+
+    elves - counter + Math.max(elves - 2 * counter, 0)
+  }
+}
