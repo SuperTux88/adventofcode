c032956588e28b36799bfdd98a9670eff79519a6
Benjamin Neff <benjamin@coding4coffee.ch>
2016-12-20 06:57:34 +0100

    day 20 2016

---
+++ src/main/scala/adventofcode/y2016/Day20.scala
@@ -0,0 +1,24 @@
+package adventofcode.y2016
+
+object Day20 extends Year2016 {
+  override val day = 20
+
+  val blacklist = input.getLines.map { line =>
+    val range = line.split("-")
+    range(0).toLong to range(1).toLong
+  }.toList.sortBy(_.start)
+
+  val (allowedIps, _) = blacklist.foldLeft(List.empty[Long], 0l) { (state, range) =>
+    val (allowedIps, lastBlocked) = state
+    val nextFree = lastBlocked + 1
+    val nextLastBlocked = Math.max(lastBlocked, range.end)
+
+    if (range.start > nextFree )
+      (allowedIps ::: (nextFree until range.start).toList, nextLastBlocked)
+    else
+      (allowedIps, nextLastBlocked)
+  }
+
+  printDayPart(1, allowedIps.head)
+  printDayPart(2, allowedIps.length)
+}
