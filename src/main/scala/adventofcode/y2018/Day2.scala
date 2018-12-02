f91e9a796f5632f1456bae17e509545f7d4c9eb3
Benjamin Neff <benjamin@coding4coffee.ch>
2018-12-02 06:53:07 +0100

    Day 2 2018
     And ignore .attach_pid* files

---
+++ src/main/scala/adventofcode/y2018/Day2.scala
@@ -0,0 +1,24 @@
+package adventofcode.y2018
+
+object Day2 extends Year2018 {
+  override val day = 2
+
+  val boxIds = input.getLines().toList
+
+  var twoTimes = 0
+  var threeTimes = 0
+
+  boxIds.foreach { id =>
+    val count = id.groupBy(identity).values.map(_.length).toList
+    if (count.contains(2)) twoTimes += 1
+    if (count.contains(3)) threeTimes += 1
+  }
+  printDayPart(1, twoTimes*threeTimes)
+
+  val similarChars = boxIds.combinations(2).collectFirst {
+    case List(a, b) if a.zip(b).count(pair => pair._1 != pair._2) == 1 =>
+      a.zip(b).filter(pair => pair._1 == pair._2).map(_._1).mkString
+  }
+
+  printDayPart(2, similarChars.get, "common part of ID: %s")
+}
