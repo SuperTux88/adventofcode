0a39c36a6e76356ce35f2e2d4097300372ee443c
Benjamin Neff <benjamin@coding4coffee.ch>
2018-12-08 08:02:08 +0100

    Day 8 2018

---
+++ src/main/scala/adventofcode/y2018/Day8.scala
@@ -0,0 +1,32 @@
+package adventofcode.y2018
+
+object Day8 extends Year2018 {
+  override val day = 8
+
+  val (metadataSum, totalValue, _) = calculateNode(input.mkString.split(" ").map(_.toInt).toList)
+
+  printDayPart(1, metadataSum)
+  printDayPart(2, totalValue)
+
+  private def calculateNode(numbers: List[Int]): (Int, Int, List[Int]) = {
+    val childrenCount :: metadataCount :: body = numbers
+
+    var remainingNumbers = body
+
+    val children = (0 until childrenCount).map { _ =>
+      val (metadataSum, nodeValue, otherNodesNumbers) = calculateNode(remainingNumbers)
+      remainingNumbers = otherNodesNumbers
+      (metadataSum, nodeValue)
+    }
+
+    val (metadata, otherNodesNumbers) = remainingNumbers.splitAt(metadataCount)
+
+    val nodeValue = if (childrenCount == 0) {
+                      metadata.sum
+                    } else {
+                      metadata.filter(_ <= children.size).map(m => children(m - 1)._2).sum
+                    }
+
+    (children.map(_._1).sum + metadata.sum, nodeValue, otherNodesNumbers)
+  }
+}
