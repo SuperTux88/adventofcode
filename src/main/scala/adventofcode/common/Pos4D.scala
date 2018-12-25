b03a51dc6896cc889a2d95a68a1d7836e5267843
Benjamin Neff <benjamin@coding4coffee.ch>
2018-12-25 06:40:57 +0100

    Day 25 2018

---
+++ src/main/scala/adventofcode/common/Pos4D.scala
@@ -0,0 +1,10 @@
+package adventofcode.common
+
+case class Pos4D(d1: Int, d2: Int, d3: Int, d4: Int) {
+  def +(direction: (Int, Int, Int, Int)): Pos4D =
+    Pos4D(d1 + direction._1, d2 + direction._2, d3 + direction._3, d4 + direction._4)
+
+  // Manhattan distance
+  def distance(other: Pos4D): Int =
+    (d1 - other.d1).abs + (d2 - other.d2).abs + (d3 - other.d3).abs + (d4 - other.d4).abs
+}
---
+++ src/main/scala/adventofcode/y2018/Day25.scala
@@ -0,0 +1,21 @@
+package adventofcode.y2018
+
+import adventofcode.common.Pos4D
+
+object Day25 extends Year2018 {
+  override val day = 25
+
+  private val PointRE = """ *(-?\d+), *(-?\d+), *(-?\d+), *(-?\d+)""".r
+
+  private val constellations = input.getLines.foldLeft(List[List[Pos4D]]()) {
+    case (const, PointRE(d1, d2, d3, d4)) =>
+      val pos = Pos4D(d1.toInt, d2.toInt, d3.toInt, d4.toInt)
+      val (matching, other) = const.partition(_.exists(_.distance(pos) <= 3))
+      if (matching.isEmpty)
+        List(pos) :: other
+      else
+        (pos :: matching.flatten) :: other
+  }
+
+  printDayPart(1, constellations.size)
+}
