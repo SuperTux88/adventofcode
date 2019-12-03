1857679ee1c72c62c6cb6a863082ee8d39d4f01c
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-03 12:54:59 +0100

    Day 3 2019 parallel
    it makes a small difference when the two wires are calculated at the
    same time :)

--- src/main/scala/adventofcode/y2019/Day3.scala
+++ src/main/scala/adventofcode/y2019/Day3.scala
@@ -2,11 +2,13 @@ package adventofcode.y2019
 
 import adventofcode.common.Pos
 
+import scala.collection.parallel.CollectionConverters._
+
 object Day3 extends Year2019 {
   override val day = 3
 
   val wires = input.getLines().take(2).map(_.split(",").map(parseInstruction).toList).toList
-  val List(wireA, wireB) = wires.map(_.foldLeft(Seq(Pos(0, 0)))(move).tail)
+  val List(wireA, wireB) = wires.par.map(_.foldLeft(Seq(Pos(0, 0)))(move).tail).toList
   val crossings = wireA intersect wireB
 
   printDayPart(1, crossings.map(_.distance(Pos(0, 0))).min)
