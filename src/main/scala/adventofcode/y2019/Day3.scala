520056dc7bffefa2b92d9abeef384b87a5137293
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-03 12:21:24 +0100

    Day 3 2019

---
+++ src/main/scala/adventofcode/y2019/Day3.scala
@@ -0,0 +1,26 @@
+package adventofcode.y2019
+
+import adventofcode.common.Pos
+
+object Day3 extends Year2019 {
+  override val day = 3
+
+  val wires = input.getLines().take(2).map(_.split(",").map(parseInstruction).toList).toList
+  val List(wireA, wireB) = wires.map(_.foldLeft(Seq(Pos(0, 0)))(move).tail)
+  val crossings = wireA intersect wireB
+
+  printDayPart(1, crossings.map(_.distance(Pos(0, 0))).min)
+
+  printDayPart(2, crossings.map(crossing => wireA.indexOf(crossing) + wireB.indexOf(crossing) + 2).min)
+
+  private def parseInstruction(instruction: String) = {
+    val (direction, distance) = instruction.splitAt(1)
+    (direction.head, distance.toInt)
+  }
+
+  private def move(positions: Seq[Pos], instruction: (Char, Int)) = {
+    val (direction, distance) = instruction
+
+    positions ++ (1 to distance).scanLeft(positions.last)((pos, _) => pos.move(direction)).tail
+  }
+}
