7ddc586fbb5381bba08df3e602edae8746fc201f
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-03 21:37:42 +0100

    Refactor day 3 2019 with iterator

--- src/main/scala/adventofcode/y2019/Day3.scala
+++ src/main/scala/adventofcode/y2019/Day3.scala
@@ -2,27 +2,27 @@ package adventofcode.y2019
 
 import adventofcode.common.Pos
 
-import scala.collection.parallel.CollectionConverters._
-
 object Day3 extends Year2019 {
   override val day = 3
 
   val wires = input.getLines().take(2).map(_.split(",").map(parseInstruction).toList).toList
-  val List(wireA, wireB) = wires.par.map(_.foldLeft(Seq(Pos(0, 0)))(move).tail).toList
+  val List(wireA, wireB) = wires.map(generatePath(_))
   val crossings = wireA intersect wireB
 
   printDayPart(1, crossings.map(_.distance(Pos(0, 0))).min)
 
   printDayPart(2, crossings.map(crossing => wireA.indexOf(crossing) + wireB.indexOf(crossing) + 2).min)
 
-  private def parseInstruction(instruction: String) = {
-    val (direction, distance) = instruction.splitAt(1)
-    (direction.head, distance.toInt)
-  }
+  private def parseInstruction(instruction: String) =
+    instruction.splitAt(1) match {
+      case (direction, distance) => (direction.head, distance.toInt)
+    }
 
-  private def move(positions: Seq[Pos], instruction: (Char, Int)) = {
-    val (direction, distance) = instruction
-
-    positions ++ (1 to distance).scanLeft(positions.last)((pos, _) => pos.move(direction)).tail
-  }
+  private def generatePath(wire: Seq[(Char, Int)]) =
+    wire.iterator.flatMap {
+      case (direction, distance) =>
+        Iterator.fill(distance)(direction)
+    }.scanLeft(Pos(0, 0)) {
+      (pos, direction) => pos.move(direction)
+    }.toList.tail
 }
