f08773230a5d6a71658e45a2168dafdae6be1f04
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-04 07:22:29 +0100

    Day 4 2019

---
+++ src/main/scala/adventofcode/y2019/Day4.scala
@@ -0,0 +1,22 @@
+package adventofcode.y2019
+
+object Day4 extends Year2019 {
+  override val day = 4
+
+  val Seq(min, max) = input.mkString.split("-").map(_.toInt).toSeq
+
+  val validPart1 = (min to max).map(_.toString.map(_.asDigit))
+    .filter(pass => isIncreasing(pass) && hasGroup(pass))
+
+  printDayPart(1, validPart1.size)
+  printDayPart(2, validPart1.count(hasGroupOfExactlyTwo))
+
+  private def isIncreasing(password: Seq[Int]) =
+    password.sliding(2).forall { case Seq(a, b) => a <= b }
+
+  private def hasGroup(password: Seq[Int]) =
+    password.sliding(2).exists { case Seq(a, b) => a == b }
+
+  private def hasGroupOfExactlyTwo(password: Seq[Int]) =
+    password.groupBy(identity).exists(_._2.size == 2)
+}
