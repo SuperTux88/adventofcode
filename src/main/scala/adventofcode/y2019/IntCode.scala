706d66f4ac471edba7750ecae6a0b13e242b1290
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-06 05:11:33 +0100

    Refactor IntCode to simplify code

--- src/main/scala/adventofcode/y2019/IntCode.scala
+++ src/main/scala/adventofcode/y2019/IntCode.scala
@@ -7,45 +7,45 @@ class IntCode(private val instructions: String) {
 
   private val program = instructions.split(",").map(_.toInt).toVector
 
-  def start(noun: Int, verb: Int): Result = run(program.updated(1, noun).updated(2, verb), 0, Vector.empty)
+  def start(noun: Int, verb: Int): Result = run(program.updated(1, noun).updated(2, verb))
   def start(input: Vector[Int]): Result = run(program, 0, input)
 
   @tailrec
-  private def run(memory: Vector[Int], ip: Int = 0, inputs: Vector[Int], outputs: Vector[Int] = Vector.empty): Result = {
+  private def run(memory: Vector[Int], ip: Int = 0, inputs: Vector[Int] = Vector.empty, outputs: Vector[Int] = Vector.empty): Result = {
+    def param(parameter: Int) = memory(ip + parameter)
+    def value(parameter: Int) =
+      (memory(ip) / pow(10, parameter + 1) % 10).toInt match {
+        case 0 => // position mode
+          memory(param(parameter))
+        case 1 => // immediate mode
+          param(parameter)
+      }
+
     memory(ip) % 100 match {
       case 1 => // add
-        val newMemory = memory.updated(memory(ip + 3), getValue(memory, ip, 1) + getValue(memory, ip, 2))
+        val newMemory = memory.updated(param(3), value(1) + value(2))
         run(newMemory, ip + 4, inputs, outputs)
       case 2 => // multiply
-        val newMemory = memory.updated(memory(ip + 3), getValue(memory, ip, 1) * getValue(memory, ip, 2))
+        val newMemory = memory.updated(param(3), value(1) * value(2))
         run(newMemory, ip + 4, inputs, outputs)
       case 3 => // input
-        run(memory.updated(memory(ip + 1), inputs.head), ip + 2, inputs.tail, outputs)
+        run(memory.updated(param(1), inputs.head), ip + 2, inputs.tail, outputs)
       case 4 => // output
-        run(memory, ip + 2, inputs, outputs :+ getValue(memory, ip, 1))
+        run(memory, ip + 2, inputs, outputs :+ value(1))
       case 5 => // jump-if-true
-        val newIp = if (getValue(memory, ip, 1) != 0) getValue(memory, ip, 2) else ip + 3
+        val newIp = if (value(1) != 0) value(2) else ip + 3
         run(memory, newIp, inputs, outputs)
       case 6 => // jump-if-false
-        val newIp = if (getValue(memory, ip, 1) == 0) getValue(memory, ip, 2) else ip + 3
+        val newIp = if (value(1) == 0) value(2) else ip + 3
         run(memory, newIp, inputs, outputs)
       case 7 => // less than
-        val newMemory = memory.updated(memory(ip + 3), if (getValue(memory, ip, 1) < getValue(memory, ip, 2)) 1 else 0)
+        val newMemory = memory.updated(param(3), if (value(1) < value(2)) 1 else 0)
         run(newMemory, ip + 4, inputs, outputs)
       case 8 => // equals
-        val newMemory = memory.updated(memory(ip + 3), if (getValue(memory, ip, 1) == getValue(memory, ip, 2)) 1 else 0)
+        val newMemory = memory.updated(param(3), if (value(1) == value(2)) 1 else 0)
         run(newMemory, ip + 4, inputs, outputs)
       case 99 => // exit
         Result(memory, outputs)
-    }
-  }
-
-  private def getValue(memory: Seq[Int], ip: Int, parameter: Int) = {
-    (memory(ip) / pow(10, parameter + 1) % 10).toInt match {
-      case 0 => // position mode
-        memory(memory(ip + parameter))
-      case 1 => // immediate mode
-        memory(ip + parameter)
     }
   }
 
