61424572aee08b42047cb3468c31d4f3b0fc9f59
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-08 09:09:36 +0100

    Use grouped instead of sliding

--- src/main/scala/adventofcode/y2019/Day8.scala
+++ src/main/scala/adventofcode/y2019/Day8.scala
@@ -9,12 +9,12 @@ object Day8 extends Year2019 {
   private val height = 6
   private val layerSize = width * height
 
-  private val encodedImage = input.map(_.asDigit).sliding(layerSize, layerSize).toSeq
+  private val encodedImage = input.map(_.asDigit).grouped(layerSize).toSeq
 
   private val fewestZeros = encodedImage.minBy(_.count(_ == 0))
   printDayPart(1, fewestZeros.count(_ == 1) * fewestZeros.count(_ == 2))
 
-  private val decodedImage = encodedImage.transpose.map(_.find(_ != 2).get).sliding(width, width).toSeq
+  private val decodedImage = encodedImage.transpose.map(_.find(_ != 2).get).grouped(width).toSeq
 
   if (Logging.debug) {
     decodedImage.foreach { line =>
@@ -25,7 +25,7 @@ object Day8 extends Year2019 {
     }
   }
 
-  private val chars = (0 until width / 5).map(pos => getCharAt(decodedImage, pos))
+  private val chars = (0 until 5).map(pos => getCharAt(decodedImage, pos))
   printDayPart(2, chars.map(OCR.readChar).mkString, "parsed password: %s")
 
   private def getCharAt(image: Seq[Seq[Int]], position: Int) = {
