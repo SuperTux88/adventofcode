8b304718cbde0158b580440c31ef394987011119
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-17 19:37:16 +0100

    Print results green and wrong result-error red

--- src/main/scala/adventofcode/DayApp.scala
+++ src/main/scala/adventofcode/DayApp.scala
@@ -2,6 +2,7 @@ package adventofcode
 
 import scala.collection.mutable
 import scala.io.{BufferedSource, Source}
+import scala.io.AnsiColor.{GREEN, RED, RESET}
 
 trait DayApp extends App {
   val day: Int
@@ -11,26 +12,28 @@ trait DayApp extends App {
 
   def printDayPart(part: Int, solution: Long, formatString: String = "%s"): Unit =
     printDayPart(part, solution.toString, formatString)
-  def printDayPart(part: Int, solution: String, formatString: String) = {
+  def printDayPart(part: Int, solution: String, formatString: String): Unit = {
     if (Logging.results) {
-      println(s"Day $day - Part $part: ${String.format(formatString, solution)}")
+      println(s"Day $day - Part $part: ${String.format(formatString, s"$GREEN$solution$RESET")}")
       Solutions.check(day, year, part, solution)
     }
   }
 
-  def printDebug(text: String) = {
+  def printDebug(text: String): Unit = {
     if (Logging.debug) println(s"DEBUG: $text")
   }
 }
+
 object Logging {
   var debug = true
   var results = true
 }
+
 object Solutions {
-  def check(day: Int, year: Int, part: Int, solution: String) = {
-    val correctSolution = solutions(year)((day-1)*2 + part-1)
+  def check(day: Int, year: Int, part: Int, solution: String): Unit = {
+    val correctSolution = solutions(year)((day - 1) * 2 + part - 1)
     if (solution != correctSolution)
-      println(s"$solution is wrong, the expected solution is: $correctSolution")
+      println(s"$RED$solution is wrong, the expected solution is: $correctSolution$RESET")
   }
 
   private val solutionsMap = mutable.Map[Int, List[String]]()
