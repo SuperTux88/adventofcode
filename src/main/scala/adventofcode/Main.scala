189af4b3f5bbca64ef8695e423d94c184d520557
Benjamin Neff <benjamin@coding4coffee.ch>
2019-12-20 16:52:52 +0100

    Day 20 2019

--- src/main/scala/adventofcode/Main.scala
+++ src/main/scala/adventofcode/Main.scala
@@ -6,7 +6,7 @@ object Main extends App {
   val allDays2015 = List(y2015.Day1, y2015.Day2, y2015.Day3, y2015.Day4, y2015.Day5, y2015.Day6, y2015.Day7, y2015.Day8, y2015.Day9, y2015.Day10, y2015.Day11, y2015.Day12, y2015.Day13, y2015.Day14, y2015.Day15, y2015.Day16, y2015.Day17, y2015.Day18, y2015.Day19, y2015.Day20, y2015.Day21, y2015.Day22, y2015.Day23, y2015.Day24, y2015.Day25)
   val allDays2016 = List(y2016.Day1, y2016.Day2, y2016.Day3, y2016.Day4, y2016.Day5, y2016.Day6, y2016.Day7, y2016.Day8, y2016.Day9, y2016.Day10, y2016.Day11, y2016.Day12, y2016.Day13, y2016.Day14, y2016.Day15, y2016.Day16, y2016.Day17, y2016.Day18, y2016.Day19, y2016.Day20, y2016.Day21, y2016.Day22, y2016.Day23, y2016.Day24, y2016.Day25)
   val allDays2018 = List(y2018.Day1, y2018.Day2, y2018.Day3, y2018.Day4, y2018.Day5, y2018.Day6, y2018.Day7, y2018.Day8, y2018.Day9, y2018.Day10, y2018.Day11, y2018.Day12, y2018.Day13, y2018.Day14, y2018.Day15, y2018.Day16, y2018.Day17, y2018.Day18, y2018.Day19, y2018.Day20, y2018.Day21, y2018.Day22, y2018.Day23, y2018.Day24, y2018.Day25)
-  val allDays2019 = List(y2019.Day1, y2019.Day2, y2019.Day3, y2019.Day4, y2019.Day5, y2019.Day6, y2019.Day7, y2019.Day8, y2019.Day9, y2019.Day10, y2019.Day11, y2019.Day12, y2019.Day13, y2019.Day14, y2019.Day15, y2019.Day16, y2019.Day17, y2019.Day18, y2019.Day19)
+  val allDays2019 = List(y2019.Day1, y2019.Day2, y2019.Day3, y2019.Day4, y2019.Day5, y2019.Day6, y2019.Day7, y2019.Day8, y2019.Day9, y2019.Day10, y2019.Day11, y2019.Day12, y2019.Day13, y2019.Day14, y2019.Day15, y2019.Day16, y2019.Day17, y2019.Day18, y2019.Day19, y2019.Day20)
 
   val years = Map("2015" -> allDays2015, "2016" -> allDays2016, "2018" -> allDays2018, "2019" -> allDays2019)
 
---
+++ src/main/scala/adventofcode/y2019/Day20.scala
@@ -0,0 +1,89 @@
+package adventofcode.y2019
+
+import adventofcode.common.{Dijkstra, Pos}
+
+import scala.annotation.tailrec
+
+object Day20 extends Year2019 {
+  override val day = 20
+
+  private val map = input.getLines().zipWithIndex.flatMap {
+    case (line, y) => line.zipWithIndex.map {
+      case (char, x) => Pos(x, y) -> char
+    }
+  }.toMap.withDefaultValue(' ')
+
+  private val labels = map.foldLeft(Map.empty[String, List[Label]].withDefaultValue(List.empty)) {
+    case (labels, (pos, char)) if char.isLetter =>
+      val downChar = map(pos.down)
+      if (downChar.isLetter) {
+        val label = if (map(pos.up) == '.')
+          Label(s"$char$downChar", pos.up, pos)
+        else
+          Label(s"$char$downChar", pos.down.down, pos.down)
+        labels + (label.name -> (label :: labels(label.name)))
+      } else {
+        val rightChar = map(pos.right)
+        if (rightChar.isLetter) {
+          val label = if (map(pos.left) == '.')
+            Label(s"$char$rightChar", pos.left, pos)
+          else
+            Label(s"$char$rightChar", pos.right.right, pos.right)
+          labels + (label.name -> (label :: labels(label.name)))
+        } else {
+          labels
+        }
+      }
+    case (labels, _) => labels
+  }
+
+  private val walls = map.filter(_._2 == '#').keys.toSet
+  private val (topLeft, bottomRight) = (Pos(2, 2), Pos(walls.map(_.x).max, walls.map(_.y).max))
+
+  private val start = labels("AA").head
+  private val end = labels("ZZ").head
+  private val portals = labels.foldLeft(Map.empty[Pos, Label]) {
+    case (portals, (_, a :: b :: Nil)) => portals ++ Map(a.labelPos -> b, b.labelPos -> a)
+    case (portals, _) => portals
+  } + (start.labelPos -> start) // start needed in portals to create start state
+
+  private val minimalStepsToExit = distanceToExit({ state: State => state.pos == end.labelPos })
+  printDayPart(1, minimalStepsToExit, "minimal steps to exit: %s")
+
+  private val minimalStepsToExitWithLevels = distanceToExit({ state: State => state == State(end.labelPos) })
+  printDayPart(2, minimalStepsToExitWithLevels, "minimal steps to exit with recursion: %s")
+
+  private def distanceToExit(isExit: State => Boolean) =
+    Dijkstra(
+      State(start.labelPos),
+      isExit,
+      { state: State =>
+        portals.get(state.pos).toList.flatMap(target =>
+          walkToLabels(List(target.pos), Set(target.labelPos)).map {
+            case (dist, pos) => (dist, State(pos, state.level + levelDiff(pos)))
+          }
+        ).filter(_._2.level >= 0) // don't go further out than outermost level
+      }
+    )._1 - 1
+
+  @tailrec
+  private def walkToLabels(positions: List[Pos], visited: Set[Pos], labels: List[(Int, Pos)] = List.empty, steps: Int = 1): List[(Int, Pos)] = {
+    if (positions.isEmpty) {
+      labels
+    } else {
+      val nextSteps = positions.flatMap { pos =>
+        Pos.directions.map(pos + _).filterNot(pos => visited.contains(pos) || map(pos) == '#')
+      }
+
+      val (foundLabels, newPositions) = nextSteps.partition(pos => map(pos).isLetter)
+      walkToLabels(newPositions, visited ++ positions, foundLabels.map((steps, _)) ::: labels, steps + 1)
+    }
+  }
+
+  private def levelDiff(pos: Pos) =
+    if (pos == start.labelPos || pos == end.labelPos) 0 // don't change levels for start and end
+    else if (topLeft.x <= pos.x && topLeft.y <= pos.y && pos.x <= bottomRight.x && pos.y <= bottomRight.y) 1 else -1
+
+  case class Label(name: String, pos: Pos, labelPos: Pos)
+  case class State(pos: Pos, level: Int = 0)
+}
