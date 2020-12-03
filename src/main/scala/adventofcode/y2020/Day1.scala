4b59fa523808d3ce0ccf5c1a9be35df285150a21
Benjamin Neff <benjamin@coding4coffee.ch>
2020-12-03 02:05:58 +0100

    Refactor Day 1 2020 to be 500 times faster \o/

--- src/main/scala/adventofcode/y2020/Day1.scala
+++ src/main/scala/adventofcode/y2020/Day1.scala
@@ -3,11 +3,14 @@ package adventofcode.y2020
 object Day1 extends Year2020 {
   override val day = 1
 
-  private val numbers = input.getLines().map(_.toInt).toSeq
+  private val numbers = input.getLines().map(_.toInt).toSeq.sorted
+  private val searchedSum = 2020
 
   printDayPart(1, productOfEntries(numbers, 2), "product of two entries: %s")
   printDayPart(2, productOfEntries(numbers, 3), "product of three entries: %s")
 
-  private def productOfEntries(numbers: Seq[Int], n: Int) =
-    numbers.combinations(n).find(_.sum == 2020).get.product
+  private def productOfEntries(numbers: Seq[Int], n: Int) = {
+    val found = numbers.combinations(n - 1).find(x => numbers.contains(searchedSum - x.sum)).get
+    found.product * (searchedSum - found.sum)
+  }
 }
