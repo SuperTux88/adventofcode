e6034a4be439d63df2507d74fae36d4a30417e68
Benjamin Neff <benjamin@coding4coffee.ch>
2020-12-09 06:39:17 +0100

    Day 9 2020

---
+++ src/main/scala/adventofcode/y2020/Day9.scala
@@ -0,0 +1,23 @@
+package adventofcode.y2020
+
+import scala.annotation.tailrec
+
+object Day9 extends Year2020 {
+  override val day = 9
+
+  private val numbers = input.getLines().map(_.toLong).toVector
+
+  private val firstWeaknessNumber = numbers.sliding(26).find { window =>
+    !window.init.combinations(2).exists(_.sum == window.last)
+  }.get.last
+
+  printDayPart(1, firstWeaknessNumber, "first number with weakness: %s")
+  printDayPart(2, findContiguousRange(numbers, 2, firstWeaknessNumber), "encryption weakness: %s")
+
+  @tailrec
+  private def findContiguousRange(numbers: Seq[Long], n: Int, searchedSum: Long): Long =
+    numbers.sliding(n).find(_.sum == searchedSum) match {
+      case None => findContiguousRange(numbers, n + 1, searchedSum)
+      case Some(range) => range.min + range.max
+    }
+}
