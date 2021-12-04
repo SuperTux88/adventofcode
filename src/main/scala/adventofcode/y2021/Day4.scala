d847e22267c5a173bac0c4952c3b86802f2fcff3
Benjamin Neff <benjamin@coding4coffee.ch>
2021-12-04 22:50:00 +0100

    Day 4 2021: Only check for bingo when a number changed
    also remove marked numbers from the board

--- src/main/scala/adventofcode/y2021/Day4.scala
+++ src/main/scala/adventofcode/y2021/Day4.scala
@@ -19,49 +19,43 @@ object Day4 extends Year2021 {
       }.toMap
     }.map(Board(_)).toSeq
 
-    val winnerBoard = play(boards, numbers)
-    printDayPart(1, winnerBoard.score, "score of the first winning board: %s")
+    val (winnerBoard, lastNumber) = play(boards, numbers)
+    printDayPart(1, calculateScore(winnerBoard, lastNumber), "score of the first winning board: %s")
 
-    val lastWinnerBoard = playAll(boards, numbers)
-    printDayPart(2, lastWinnerBoard.score, "score of the last winning board: %s")
+    val (lastWinnerBoard, lastWinningNumber) = playAll(boards, numbers)
+    printDayPart(2, calculateScore(lastWinnerBoard, lastWinningNumber), "score of the last winning board: %s")
   }
 
   @tailrec
-  private def play(boards: Seq[Board], numbers: List[Int]): Board = {
+  private def play(boards: Seq[Board], numbers: List[Int]): (Board, Int) = {
     val (number :: remaining) = numbers
     val newBoards = boards.map(_.mark(number))
-    newBoards.find(_.isBingo) match {
-      case Some(bingoBoard) => bingoBoard
-      case None => play(newBoards, remaining)
+    newBoards.find(_._2) match {
+      case Some(bingoBoard, true) => (bingoBoard, number)
+      case _ => play(newBoards.map(_._1), remaining)
     }
   }
 
   @tailrec
-  private def playAll(boards: Seq[Board], numbers: List[Int]): Board = {
+  private def playAll(boards: Seq[Board], numbers: List[Int]): (Board, Int) = {
     val (number :: remaining) = numbers
     boards.map(_.mark(number)) match {
-      case Seq(last) => last
-      case newBoards => playAll(newBoards.filterNot(_.isBingo), remaining)
+      case Seq((lastBoard, true)) => (lastBoard, number)
+      case newBoards => playAll(newBoards.filterNot(_._2).map(_._1), remaining)
     }
   }
 
-  private case class Board(numbers: Map[Int, Pos], marked: List[Pos] = List.empty) {
+  private def calculateScore(board: Board, lastNumber: Int) = board.numbers.keys.sum * lastNumber
+
+  private case class Board(numbers: Map[Int, Pos]) {
     def mark(number: Int) =
       numbers.get(number) match {
-        case Some(pos) => copy(marked = pos :: marked)
-        case None => this
+        case Some(pos) =>
+          val newBoard = copy(numbers = numbers - number)
+          (newBoard, newBoard.isBingo(pos))
+        case None => (this, false)
       }
 
-    def isBingo =
-      (0 until 5).exists { index =>
-        (0 until 5).forall(x => marked.contains(Pos(x, index))) ||
-          (0 until 5).forall(y => marked.contains(Pos(index, y)))
-      }
-
-    def score = {
-      val lastNumber = numbers.find(_._2 == marked.head).get._1
-      val unmarkedValues = numbers.filter(num => !marked.contains(num._2)).map(_._1)
-      unmarkedValues.sum * lastNumber
-    }
+    private def isBingo(pos: Pos) = !numbers.values.exists(_.x == pos.x) || !numbers.values.exists(_.y == pos.y)
   }
 }
