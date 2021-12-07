44f3d5925ffcc1c31fe655886039a16539d5202a
Benjamin Neff <benjamin@coding4coffee.ch>
2021-12-07 04:22:28 +0100

    Day 6 2021: Use Vector instead of Map for perDaysRemaining

--- src/main/scala/adventofcode/y2021/Day6.scala
+++ src/main/scala/adventofcode/y2021/Day6.scala
@@ -1,26 +1,21 @@
 package adventofcode.y2021
 
-import adventofcode.common.IterableImplicits
-
 object Day6 extends Year2021 {
   override val day = 6
 
   override def runDay(input: String): Unit = {
-    val perDaysRemaining = input.split(",").toIterable.groupCount(_.toInt)
+    val fishes = input.split(",").map(_.toInt).toSeq
+    val perDaysRemaining = Vector.tabulate(9)(days => fishes.count(_ == days).toLong)
 
     val after80Days = (0 until 80).foldLeft(perDaysRemaining)((currentPerDays, _) => calculateDay(currentPerDays))
-    printDayPart(1, after80Days.values.sum, "there are lanternfish %s after 80 days")
+    printDayPart(1, after80Days.sum, "there are lanternfish %s after 80 days")
 
     val after256Days = (80 until 256).foldLeft(after80Days)((currentPerDays, _) => calculateDay(currentPerDays))
-    printDayPart(2, after256Days.values.sum, "there are lanternfish %s after 256 days")
+    printDayPart(2, after256Days.sum, "there are lanternfish %s after 256 days")
   }
 
-  private def calculateDay(perDaysRemaining: Map[Int, Long]) = {
-    perDaysRemaining.foldLeft(Map[Int, Long]().withDefaultValue(0L)) { (state, daysRemaining) =>
-      daysRemaining match {
-        case (0, count) => state.updated(6, state(6) + count).updated(8, count)
-        case (remaining, count) => state.updated(remaining - 1, state(remaining - 1) + count)
-      }
-    }
+  private def calculateDay(perDaysRemaining: Vector[Long]) = {
+    val zeroDays +: nonZeroDays = perDaysRemaining
+    nonZeroDays.updated(6, nonZeroDays(6) + zeroDays) :+ zeroDays
   }
 }
