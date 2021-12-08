fdf7abbdd6c4461d0d3f24c4c86bca1e7d0e7b5c
Benjamin Neff <benjamin@coding4coffee.ch>
2021-12-08 01:53:48 +0100

    Day 7 2021: Part 2 is within +/- 0.5 of the mean
    so only need to check the mean and the number above (since the mean
    calculated as a integer is always rounded down, so it can't be lower
    than that)

--- src/main/scala/adventofcode/y2021/Day7.scala
+++ src/main/scala/adventofcode/y2021/Day7.scala
@@ -2,8 +2,6 @@ package adventofcode.y2021
 
 import adventofcode.common.NumberHelper.increasingSum
 
-import scala.annotation.tailrec
-
 object Day7 extends Year2021 {
   override val day = 7
 
@@ -14,24 +12,7 @@ object Day7 extends Year2021 {
     printDayPart(1, crabs.map(c => math.abs(c - median)).sum, "fuel spent to align positions: %s")
 
     val mean = crabs.sum / crabs.size
-    val minFuelCost = getInitialDirection(crabs, mean) match {
-      case (meanFuelCost, 0) => meanFuelCost
-      case (meanFuelCost, direction) => searchBestPosition(crabs, mean, meanFuelCost, direction)
-    }
+    val minFuelCost = Seq(0, 1).map(d => crabs.map(c => increasingSum(math.abs(c -  (mean + d)))).sum).min
     printDayPart(2, minFuelCost, "fuel spent to align positions with increasing costs: %s")
-  }
-
-  private def increasingFuelCostToTarget(crabs: Seq[Int], target: Int) =
-    crabs.map(c => increasingSum(math.abs(c - target))).sum
-
-  private def getInitialDirection(crabs: Seq[Int], target: Int) = {
-    val costs = Seq(-1, 0, +1).map(d => increasingFuelCostToTarget(crabs, target + d))
-    (costs(1), costs.indexOf(costs.min) - 1)
-  }
-
-  @tailrec
-  private def searchBestPosition(crabs: Seq[Int], target: Int, fuelCost: Int, direction: Int): Int = {
-    val newFuelCost = increasingFuelCostToTarget(crabs, target + direction)
-    if newFuelCost > fuelCost then fuelCost else searchBestPosition(crabs, target + direction, newFuelCost, direction)
   }
 }
