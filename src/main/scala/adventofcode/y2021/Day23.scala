09a4ee6b428da015bc7ef5abc6d07b0a1adc41b8
Benjamin Neff <benjamin@coding4coffee.ch>
2021-12-24 01:55:58 +0100

    Day 23 2021: Refactor with lists for the rooms

--- src/main/scala/adventofcode/y2021/Day23.scala
+++ src/main/scala/adventofcode/y2021/Day23.scala
@@ -9,58 +9,70 @@ import scala.io.BufferedSource
 object Day23 extends Year2021 {
   override val day = 23
 
-  private val COSTS = Map('A' -> 1, 'B' -> 10, 'C' -> 100, 'D' -> 1000)
-  private val FOLDED = Map(
-    Pos(3, 3) -> 'D', Pos(5, 3) -> 'C', Pos(7, 3) -> 'B', Pos(9, 3) -> 'A',
-    Pos(3, 4) -> 'D', Pos(5, 4) -> 'B', Pos(7, 4) -> 'A', Pos(9, 4) -> 'C'
-  )
-
-  private val HALLWAY_POSITIONS = Set(Pos(1, 1), Pos(2, 1), Pos(4, 1), Pos(6, 1), Pos(8, 1), Pos(10, 1), Pos(11, 1))
-  private val FIRST_ROW = 2
-  private val COLS = COSTS.keys.zip(List(3, 5, 7, 9)).toMap
+  private val HALLWAY_INDEXES = Set(1, 2, 4, 6, 8, 10, 11)
+  private val TARGET_ROOMS = Map(Amber() -> 3, Bronze() -> 5, Copper() -> 7, Desert() -> 9)
 
   override def runDay(input: BufferedSource): Unit = {
-    val startPositions = Pos.parseMap(input.getLines().takeWhile(_.nonEmpty), identity)
-      .filter(pos => COSTS.keySet.contains(pos._2))
+    val initialRooms = parseLines(input.getLines().takeWhile(_.nonEmpty).toList)
 
-    printDayPart(1, getLowestEnergy(startPositions), "least energy required to organize the amphipods: %s")
+    printDayPart(1, getLowestEnergy(initialRooms), "least energy required to organize the amphipods: %s")
 
-    val unfolded = startPositions.filter(_._1.y == FIRST_ROW) ++ FOLDED
-      ++ startPositions.filterNot(_._1.y == FIRST_ROW).map { case (p, a) => p + (0, 2) -> a }
-    printDayPart(2, getLowestEnergy(unfolded), "least energy required to organize the amphipods after unfold: %s")
+    val foldedLines =
+      """  #D#C#B#A#
+        |  #D#B#A#C#""".stripMargin.linesIterator.toList
+    val unfoldedRooms = parseLines(foldedLines)
+      .map { case (index, folded) => index -> (initialRooms(index).head :: folded ::: initialRooms(index).tail) }
+    printDayPart(2, getLowestEnergy(unfoldedRooms, 4), "least energy required to organize the amphipods after unfold: %s")
   }
 
-  private def getLowestEnergy(startPositions: Map[Pos, Char]): Int = {
-    val rows = 2 to startPositions.map(_._1.y).max
-    val goal = COLS.flatMap { case (a, x) => rows.map(y => Pos(x, y) -> a) }
+  private def getLowestEnergy(startRooms: Map[Int, List[Amphipod]], roomSize: Int = 2): Int = {
+    val targetRooms = TARGET_ROOMS.map { case (a, i) => i -> List.fill(roomSize)(a) }
     Dijkstra(
-      startPositions,
-      state => state == goal,
-      state => getMoveOutStates(state, rows) ::: getMoveInStates(state, rows)
+      State(startRooms, Map.empty),
+      _.rooms == targetRooms,
+      state => getMoveOutStates(state, roomSize) ::: getMoveInStates(state, roomSize)
     )._1
   }
 
-  private def getMoveOutStates(state: Map[Pos, Char], rows: Range): List[(Int, Map[Pos, Char])] =
-    state.filter { case (pos, amphipod) =>
-      !HALLWAY_POSITIONS.contains(pos)
-        && freeAbove(state, rows.start, pos)
-        && !completedBelow(state, rows.end, pos, amphipod)
-    }.toList.flatMap { case (from, amphipod) => HALLWAY_POSITIONS
-      .filterNot(target => rangeWithReverse(from.x, target.x).exists(x => state.contains(Pos(x, target.y))))
-      .map(target => (from.distance(target) * COSTS(amphipod), state.removed(from).updated(target, amphipod)))
+  private def getMoveOutStates(state: State, roomSize: Int): List[(Int, State)] =
+    state.rooms.filterNot { case (pos, room) => room.forall(TARGET_ROOMS(_) == pos) }.toList.flatMap {
+      case (roomPos, room) => HALLWAY_INDEXES
+        .filter(hallwayPos => rangeWithReverse(roomPos, hallwayPos).forall(p => !state.hallway.contains(p)))
+        .map { hallwayPos =>
+          ((roomSize - room.size + 1 + (roomPos - hallwayPos).abs) * room.head.energy,
+            State(state.rooms.updated(roomPos, room.tail), state.hallway.updated(hallwayPos, room.head)))
+        }
     }
 
-  private def getMoveInStates(state: Map[Pos, Char], rows: Range): List[(Int, Map[Pos, Char])] =
-    state.filter(pos => HALLWAY_POSITIONS.contains(pos._1)).toList.flatMap { case (from, amphipod) =>
-      rows.map(Pos(COLS(amphipod), _)).find { target =>
-        freeAbove(state, rows.start, target) && completedBelow(state, rows.end, target, amphipod)
-          && !rangeWithReverse(target.x, from.x, false).exists(x => state.contains(Pos(x, from.y)))
-      }.map(target => (from.distance(target) * COSTS(amphipod), state.removed(from).updated(target, amphipod)))
+  private def getMoveInStates(state: State, roomSize: Int): List[(Int, State)] =
+    state.hallway.toList.flatMap {
+      case (hallwayPos, amphipod) =>
+        val roomPos = TARGET_ROOMS(amphipod)
+        val room = state.rooms(roomPos)
+        if (room.forall(_ == amphipod)
+          && rangeWithReverse(roomPos, hallwayPos, false).forall(p => !state.hallway.contains(p)))
+          Some((roomSize - room.size + (roomPos - hallwayPos).abs) * amphipod.energy,
+            State(state.rooms.updated(roomPos, amphipod :: room), state.hallway.removed(hallwayPos)))
+        else
+          None
     }
 
-  private def freeAbove(state: Map[Pos, Char], firstRow: Int, pos: Pos) =
-    !(firstRow until pos.y).exists(y => state.contains(Pos(pos.x, y)))
+  private def parseLines(lines: List[String]) =
+    lines.reverse.foldLeft(TARGET_ROOMS.values.map(_ -> List.empty[Amphipod]).toMap) { (rooms, line) =>
+      line.zipWithIndex.foldLeft(rooms) {
+        case (rooms, ('A', x)) => rooms.updated(x, Amber() :: rooms(x))
+        case (rooms, ('B', x)) => rooms.updated(x, Bronze() :: rooms(x))
+        case (rooms, ('C', x)) => rooms.updated(x, Copper() :: rooms(x))
+        case (rooms, ('D', x)) => rooms.updated(x, Desert() :: rooms(x))
+        case (rooms, _) => rooms
+      }
+    }
 
-  private def completedBelow(state: Map[Pos, Char], lastRow: Int, pos: Pos, amphipod: Char) =
-    COLS(amphipod) == pos.x && (pos.y + 1 to lastRow).forall(y => state.get(Pos(pos.x, y)).contains(amphipod))
+  private sealed trait Amphipod(val energy: Int)
+  private case class Amber() extends Amphipod(1)
+  private case class Bronze() extends Amphipod(10)
+  private case class Copper() extends Amphipod(100)
+  private case class Desert() extends Amphipod(1000)
+
+  private case class State(rooms: Map[Int, List[Amphipod]], hallway: Map[Int, Amphipod])
 }
