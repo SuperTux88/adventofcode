7c3be4b293eac9159b93b0ff10905616c96e3346
Benjamin Neff <benjamin@coding4coffee.ch>
2022-12-21 00:19:09 +0100

    Completely rewrite day 19 2022 for the third time ...
    This time it at least works again for both parts, but there is probably
    still room for optimization, but I don't want to break it again :D

--- src/main/scala/adventofcode/y2022/Day19.scala
+++ src/main/scala/adventofcode/y2022/Day19.scala
@@ -1,5 +1,6 @@
 package adventofcode.y2022
 
+import scala.annotation.tailrec
 import scala.collection.parallel.CollectionConverters.*
 import scala.io.BufferedSource
 
@@ -16,25 +17,24 @@ object Day19 extends Year2022 {
         )
     }.toList
 
-    val maxGeodes24 = blueprints.par.map(b => findBest(b) * b.id)
-    printDayPart(1, maxGeodes24.sum, "Sum of all quality level: %s")
+    val qualityLevels = blueprints.par.map(b => findBest(b) * b.id)
+    printDayPart(1, qualityLevels.sum, "Sum of all quality level: %s")
 
     val maxGeodes32 = blueprints.take(3).par.map(b => findBest(b, 32))
-    println(maxGeodes32)
     printDayPart(2, maxGeodes32.product, "Largest numbers of geodes multiplied together: %s")
   }
 
-  private def findBest(blueprint: Blueprint, minutes: Int = 24): Int =
-    (1 to minutes).foldLeft(List(Resources())) { (states, _) =>
-      val newStates = states.flatMap(_.canBuy(blueprint))
-      (maxRes(newStates, _.clayBots), maxRes(newStates, _.obsidianBots), maxRes(newStates, _.geodeBots)) match {
-        case (maxClay, 0, 0) => newStates.filter(_.clayBots >= maxClay - 1)
-        case (_, maxObsidian, 0) => newStates.filter(_.obsidianBots >= maxObsidian - 1)
-        case (_, _, maxGeode) => newStates.filter(_.geodeBots == maxGeode)
-      }
-    }.maxBy(_.geode).geode
+  private def findBest(blueprint: Blueprint, minutes: Int = 24): Int = {
+    @tailrec
+    def find(states: List[State], best: Int = 0): Int =
+      if (states.nonEmpty) {
+        val newStates = states.flatMap(_.next(blueprint))
+        val newBest = best max newStates.maxByOption(_.totalGeodes).map(_.totalGeodes).getOrElse(0)
+        find(newStates.filter(_.canBeat(newBest)), newBest)
+      } else best
 
-  private def maxRes(res: List[Resources], by: Resources => Int) = by(res.maxBy(by))
+    find(List(State(minutes)))
+  }
 
   private case class Costs(oreOre: Int, clayOre: Int,
                            obsidianOre: Int, obsidianClay: Int,
@@ -42,48 +42,70 @@ object Day19 extends Year2022 {
     val maxOre: Int = Seq(oreOre, clayOre, obsidianOre, geodeOre).max
   }
 
-  private case class Resources(ore: Int = 0, clay: Int = 0, obsidian: Int = 0, geode: Int = 0,
-                               oreBots: Int = 1, clayBots: Int = 0, obsidianBots: Int = 0, geodeBots: Int = 0) {
-    def canBuy(blueprint: Blueprint): List[Resources] =
-      buyGeodeBotOption(blueprint) match {
-        case Some(geodeBot) => List(geodeBot)
-        case None =>
-          val buyBots = List(buyOreBotOption(blueprint), buyClayBotOption(blueprint), buyObsidianBotOption(blueprint))
-          next :: buyBots.flatten
-      }
-
-    private def next = copy(ore = ore + oreBots, clay = clay + clayBots, obsidian = obsidian + obsidianBots, geode = geode + geodeBots)
-
-    private def buyOreBotOption(blueprint: Blueprint) =
-      if (ore >= blueprint.costs.oreOre && oreBots < blueprint.costs.maxOre)
-        Some(next.buyOreBot(blueprint))
-      else None
-
-    private def buyOreBot(blueprint: Blueprint) = copy(ore = ore - blueprint.costs.oreOre, oreBots = oreBots + 1)
-
-    private def buyClayBotOption(blueprint: Blueprint) =
-      if (ore >= blueprint.costs.clayOre)
-        Some(next.buyClayBot(blueprint))
-      else None
-
-    private def buyClayBot(blueprint: Blueprint) = copy(ore = ore - blueprint.costs.clayOre, clayBots = clayBots + 1)
-
-    private def buyObsidianBotOption(blueprint: Blueprint) =
-      if (ore >= blueprint.costs.obsidianOre && clay >= blueprint.costs.obsidianClay)
-        Some(next.buyObsidianBot(blueprint))
-      else None
-
-    private def buyObsidianBot(blueprint: Blueprint) =
-      copy(ore = ore - blueprint.costs.obsidianOre, clay = clay - blueprint.costs.obsidianClay, obsidianBots = obsidianBots + 1)
-
-    private def buyGeodeBotOption(blueprint: Blueprint) =
-      if (ore >= blueprint.costs.geodeOre && obsidian >= blueprint.costs.geodeObsidian)
-        Some(next.buyGeodeBot(blueprint))
-      else None
-
-    private def buyGeodeBot(blueprint: Blueprint) =
-      copy(ore = ore - blueprint.costs.geodeOre, obsidian = obsidian - blueprint.costs.geodeObsidian, geodeBots = geodeBots + 1)
-  }
-
   private case class Blueprint(id: Int, costs: Costs)
+
+  private case class State(remaining: Int, totalGeodes: Int = 0,
+                           ore: Int = 0, clay: Int = 0, obsidian: Int = 0,
+                           oreBots: Int = 1, clayBots: Int = 0, obsidianBots: Int = 0) {
+
+    // filter all states that can't beat the current best
+    // (even if it would be possible to buy geodes every minute from now on)
+    def canBeat(geodes: Int): Boolean = (1 to remaining).sum + totalGeodes >= geodes
+
+    def next(blueprint: Blueprint): List[State] =
+      List(oreBot(blueprint), clayBot(blueprint), obsidianBot(blueprint), geodeBot(blueprint)).flatten
+
+    private def run(minutes: Int) = if (minutes < remaining) Some(copy(
+      remaining = remaining - minutes,
+      ore = ore + oreBots * minutes,
+      clay = clay + clayBots * minutes,
+      obsidian = obsidian + obsidianBots * minutes
+    )) else None
+
+    private def oreBot(b: Blueprint) = {
+      if (oreBots < b.costs.maxOre && ore + oreBots * remaining > b.costs.oreOre) {
+        val minutes = requiredMinutes(b.costs.oreOre, ore, oreBots)
+        run(minutes + 1).map(s => s.copy(ore = s.ore - b.costs.oreOre, oreBots = oreBots + 1))
+      } else None
+    }
+
+    private def clayBot(b: Blueprint) = {
+      if (clayBots < b.costs.obsidianClay && ore + oreBots * remaining > b.costs.clayOre) {
+        val minutes = requiredMinutes(b.costs.clayOre, ore, oreBots)
+        run(minutes + 1).map(s => s.copy(ore = s.ore - b.costs.clayOre, clayBots = clayBots + 1))
+      } else None
+    }
+
+    private def obsidianBot(b: Blueprint) = {
+      if (obsidianBots < b.costs.geodeObsidian
+        && ore + oreBots * remaining > b.costs.obsidianOre && clay + clayBots * remaining > b.costs.obsidianClay) {
+        val oreMinutes = requiredMinutes(b.costs.obsidianOre, ore, oreBots)
+        val clayMinutes = requiredMinutes(b.costs.obsidianClay, clay, clayBots)
+        run((oreMinutes max clayMinutes) + 1).map(s => s.copy(
+          ore = s.ore - b.costs.obsidianOre,
+          clay = s.clay - b.costs.obsidianClay,
+          obsidianBots = obsidianBots + 1
+        ))
+      } else None
+    }
+
+    private def geodeBot(b: Blueprint) = {
+      if (ore + oreBots * remaining > b.costs.geodeOre && obsidian + obsidianBots * remaining > b.costs.geodeObsidian) {
+        val oreMinutes = requiredMinutes(b.costs.geodeOre, ore, oreBots)
+        val obsidianMinutes = requiredMinutes(b.costs.geodeObsidian, obsidian, obsidianBots)
+        val minutes = (oreMinutes max obsidianMinutes) + 1
+        run(minutes).map(s => s.copy(
+          ore = s.ore - b.costs.geodeOre,
+          obsidian = s.obsidian - b.costs.geodeObsidian,
+          totalGeodes = totalGeodes + (remaining - minutes)
+        ))
+      } else None
+    }
+
+    private def requiredMinutes(cost: Int, available: Int, bots: Int) =
+      if (available < cost) {
+        val requiredOre = cost - available
+        requiredOre / bots + (if requiredOre % bots == 0 then 0 else 1)
+      } else 0
+  }
 }
