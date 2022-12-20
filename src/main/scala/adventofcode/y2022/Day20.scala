34f3a5db0e2b0b54468eef43797c7b16550af799
Benjamin Neff <benjamin@coding4coffee.ch>
2022-12-20 08:40:37 +0100

    Optimize day 20 2022 with mutable ArrayBuffer

--- src/main/scala/adventofcode/y2022/Day20.scala
+++ src/main/scala/adventofcode/y2022/Day20.scala
@@ -1,33 +1,40 @@
 package adventofcode.y2022
 
-import scala.io.BufferedSource
 import adventofcode.common.NumberHelper.ExtendedIntegral
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.BufferedSource
 
 object Day20 extends Year2022 {
   override val day = 20
 
+  private val DECRYPTION_KEY = 811589153L
+
   override def runDay(input: BufferedSource): Unit = {
     val numbers = input.getLines().zipWithIndex.map((l, i) => Number(l.toLong, i)).toVector
 
-    val shuffled = numbers.foldLeft(numbers)(moveNumber)
+    val numBuffer = ArrayBuffer.from(numbers)
+    numbers.foreach(n => moveNumberMutable(numBuffer, n))
 
-    printDayPart(1, getGroveCoordinates(shuffled).sum, "Sum of grove coordinates: %s")
+    printDayPart(1, getGroveCoordinates(numBuffer).sum, "Sum of grove coordinates: %s")
 
-    val decrypted = numbers.map(n => n.copy(value = n.value * 811589153L))
-    val shuffled10 = (1 to 10).foldLeft(decrypted) { (nums, _) =>
-      decrypted.foldLeft(nums)(moveNumber)
+    val decrypted = numbers.map(n => n.copy(value = n.value * DECRYPTION_KEY))
+    val decryptedBuffer = ArrayBuffer.from(decrypted)
+    (1 to 10).foreach { _ =>
+      decrypted.foreach(n => moveNumberMutable(decryptedBuffer, n))
     }
 
-    printDayPart(2, getGroveCoordinates(shuffled10).sum, "Sum of grove coordinates with decription key: %s")
+    printDayPart(2, getGroveCoordinates(decryptedBuffer).sum, "Sum of grove coordinates with decription key: %s")
   }
 
-  private def moveNumber(numbers: Vector[Number], number: Number): Vector[Number] = {
-    val without = numbers.filterNot(_ == number)
-    val (before, after) = without.splitAt(((numbers.indexOf(number) + number.value) %+ without.size).toInt)
-    before ++ Vector(number) ++ after
+  private def moveNumberMutable(numbers: ArrayBuffer[Number], number: Number): Unit = {
+    val idx = numbers.indexWhere(_ == number)
+    numbers.remove(idx)
+    val insertIdx = (idx + number.value) %+ numbers.length
+    numbers.insert(insertIdx.toInt, number)
   }
 
-  private def getGroveCoordinates(numbers: Vector[Number]): List[Long] = {
+  private def getGroveCoordinates(numbers: ArrayBuffer[Number]): List[Long] = {
     val zero = numbers.indexWhere(_.value == 0)
     List(1000, 2000, 3000).map(i => numbers((zero + i) % numbers.size).value)
   }
