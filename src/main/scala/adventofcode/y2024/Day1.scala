94e56d226c95a5727fbb5e63a6cda25988be642e
Benjamin Neff <benjamin@coding4coffee.ch>
2024-12-01 14:48:30 +0100

    Rename variables in day 1 2024 to left/right to match puzzle text

--- src/main/scala/adventofcode/y2024/Day1.scala
+++ src/main/scala/adventofcode/y2024/Day1.scala
@@ -10,17 +10,17 @@ object Day1 extends Year2024 {
   private val NumbersRE = """(\d+)\s+(\d+)""".r
 
   override def runDay(input: BufferedSource): Unit = {
-    val (list1, list2) = input.getLines().takeWhile(_.nonEmpty).map {
-      case NumbersRE(a, b) => (a.toInt, b.toInt)
+    val (left, right) = input.getLines().takeWhile(_.nonEmpty).map {
+      case NumbersRE(l, r) => (l.toInt, r.toInt)
     }.toSeq.unzip
 
-    val distances = list1.sorted.zip(list2.sorted).map { case (a, b) =>
+    val distances = left.sorted.zip(right.sorted).map { case (a, b) =>
       (a - b).abs
     }
     printDayPart(1, distances.sum, "Distance between the two lists: %s")
 
-    val list2Counts = list2.groupCount(identity)
-    val similarity = list1.map(x => list2Counts.getOrElse(x, 0) * x)
+    val rightCounts = right.groupCount(identity)
+    val similarity = left.map(l => rightCounts.getOrElse(l, 0) * l)
     printDayPart(2, similarity.sum, "Similarity score of the two lists: %s")
   }
 }
