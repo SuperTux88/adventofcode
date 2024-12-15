868a1c67cec49a706d7b1a7509cc6bfa8bb21958
Benjamin Neff <benjamin@coding4coffee.ch>
2024-12-16 00:42:26 +0100

    Optimize day 14 2024 by looping in bigger steps
    Same optimization as in my rust solution:
    https://github.com/SuperTux88/adventofcode-rs/commit/f9d5de62e259bcf536b2dccffabe7520b05b519d

--- src/main/scala/adventofcode/y2024/Day14.scala
+++ src/main/scala/adventofcode/y2024/Day14.scala
@@ -17,12 +17,14 @@ object Day14 extends Year2024 {
   private val QUADRANTS_BORDER = SIZE / 2
   private val QUADRANTS = QUADRANTS_BORDER + Pos(1, 1)
 
+  private val OUTLIER_THRESHOLD = 30 // how many robots on the same row/col
+
   override def runDay(input: BufferedSource): Unit = {
     val robots = input.getLines().takeWhile(_.nonEmpty).map {
       case RobotRE(px, py, vx, vy) => Robot(Pos(px.toInt, py.toInt), Pos(vx.toInt, vy.toInt))
     }.toSeq
 
-    val robots100 = (1 to 100).foldLeft(robots)((robots, _) => robots.map(_.move))
+    val robots100 = (1 to 100).foldLeft(robots)((robots, _) => robots.map(_.move()))
     printMap(robots100)
 
     val quadrants = robots100.flatMap(_.quadrant).groupBy(identity).values.map(_.size)
@@ -33,17 +35,31 @@ object Day14 extends Year2024 {
     printDayPart(2, seconds, "Fewest numbers of seconds to display easter egg: %s")
   }
 
-  @tailrec
-  private def findTree(robots: Seq[Robot], seconds: Int = 1): (Int, Seq[Robot]) = {
-    val newRobots = robots.map(_.move)
-    if (!newRobots.groupBy(_.p).values.exists(_.size > 1))
-      (seconds, newRobots)
-    else
-      findTree(newRobots, seconds + 1)
+  private def findTree(robots: Seq[Robot]): (Int, Seq[Robot]) = {
+    @tailrec
+    def findOutlier(robots: Seq[Robot], seconds: Int = 1): (Int, Seq[Robot]) = {
+      val newRobots = robots.map(_.move())
+      if (newRobots.groupBy(_.p.x).values.exists(_.size > OUTLIER_THRESHOLD))
+        (seconds, newRobots)
+      else
+        findOutlier(newRobots, seconds + 1)
+    }
+
+    @tailrec
+    def search(robots: Seq[Robot], seconds: Int): (Int, Seq[Robot]) = {
+      val newRobots = robots.map(_.move(SIZE.x))
+      if (newRobots.groupBy(_.p).values.exists(_.size > 1))
+        search(newRobots, seconds + SIZE.x)
+      else
+        (seconds + SIZE.x, newRobots)
+    }
+
+    val (seconds, outlierRobots) = findOutlier(robots)
+    search(outlierRobots, seconds)
   }
 
   private case class Robot(p: Pos, v: Pos) {
-    def move: Robot = this.copy(p = (p + v) %+ SIZE)
+    def move(steps: Int = 1): Robot = this.copy(p = (p + v * steps) %+ SIZE)
     def quadrant: Option[Pos] =
       if (p.x == QUADRANTS_BORDER.x || p.y == QUADRANTS_BORDER.y) None
       else Some(p / QUADRANTS)
