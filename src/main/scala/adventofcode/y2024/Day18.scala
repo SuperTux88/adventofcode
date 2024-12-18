9535c182422fd90cd176a2b71e7c60e1aa7aadaa
Benjamin Neff <benjamin@coding4coffee.ch>
2024-12-18 06:35:40 +0100

    Day 18 2024

---
+++ src/main/scala/adventofcode/y2024/Day18.scala
@@ -0,0 +1,49 @@
+package adventofcode.y2024
+
+import adventofcode.Logging
+import adventofcode.common.pos.Pos
+import adventofcode.common.search.Dijkstra
+
+import scala.io.BufferedSource
+
+object Day18 extends Year2024 {
+  override val day = 18
+
+  // private val SIZE = Pos(6, 6)
+  // private val BYTES = 12
+
+  private val SIZE = Pos(70, 70)
+  private val BYTES = 1024
+
+  override def runDay(input: BufferedSource): Unit = {
+    val bytes = input.getLines().takeWhile(_.nonEmpty).map { line =>
+      line.split(",") match {
+        case Array(x, y) => Pos(x.toInt, y.toInt)
+      }
+    }.toList
+
+    val firstBytes = bytes.take(BYTES)
+    val (_, steps) = Dijkstra(Pos(0, 0), _ == SIZE, next(firstBytes))
+    printMap(firstBytes, steps)
+    printDayPart(1, steps.size - 1, "P1: %s")
+
+    val index = bytes.indices.findLast { i =>
+      Dijkstra(Pos(0, 0), _ == SIZE, next(bytes.take(i)))._2.nonEmpty
+    }.get
+    if (Logging.debug) printMap(bytes.take(index), Dijkstra(Pos(0, 0), _ == SIZE, next(bytes.take(index)))._2)
+    printDayPart(2, bytes(index).toString, "P2: %s")
+  }
+
+  private def next(bytes: List[Pos])(pos: Pos): List[(Int, Pos)] =
+    pos.directions.filter { next =>
+      next.x >= 0 && next.x <= SIZE.x && next.y >= 0 && next.y <= SIZE.y && !bytes.contains(next)
+    }.map((1, _))
+
+  private def printMap(bytes: List[Pos], steps: List[Pos]): Unit = if (Logging.debug)
+    for (y <- 0 to SIZE.y) {
+      for (x <- 0 to SIZE.x) {
+        print(if (bytes.contains(Pos(x, y))) '#' else if (steps.contains(Pos(x, y))) 'O' else '.')
+      }
+      println()
+    }
+}
